2a7d99e1ef9fa745d2a30a65fbd4f42269d352a6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-20 14:45:48 +0100

    Added new run log

--- load_journal.py
+++ load_journal.py
@@ -1,5 +1,8 @@
-from spectrum import load
+from spectrum import load, logger
+
+LOGGER = logger.logger(__name__)
 
 if __name__ == '__main__':
     while True:
+        LOGGER.info("New run")
         load.JOURNAL_ALL.run()
