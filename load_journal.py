f43ce1f2021a13f191d9b51d2a6f5488768a7f42
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-21 16:19:33 +0100

    Logging exceptions rather than failing fast

--- load_journal.py
+++ load_journal.py
@@ -3,4 +3,7 @@ from spectrum import load
 if __name__ == '__main__':
     while True:
         load.LOGGER.info("New run")
-        load.JOURNAL_ALL.run()
+        try:
+            load.JOURNAL_ALL.run()
+        except (AssertionError, RuntimeError, UnrecoverableException, ValueError):
+            load.LOGGER.exception()
