edf403eafb782db86ffaf89b01195e4ca898a3c6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-21 16:22:07 +0100

    Renaming exceptions to standard Error suffix

--- load_journal.py
+++ load_journal.py
@@ -1,9 +1,9 @@
-from spectrum import load
+from spectrum import load, checks
 
 if __name__ == '__main__':
     while True:
         load.LOGGER.info("New run")
         try:
             load.JOURNAL_ALL.run()
-        except (AssertionError, RuntimeError, UnrecoverableException, ValueError):
-            load.LOGGER.exception()
+        except (AssertionError, RuntimeError, ValueError, checks.UnrecoverableError):
+            load.LOGGER.exception("Error in loading a journal page")
