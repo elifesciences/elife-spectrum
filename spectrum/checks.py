90cb3de7a3d23cfe1dd83cd7aa49a66cd79d97e9
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-20 17:17:41 +0100

    Load /profiles as a sanity check

--- spectrum/checks.py
+++ spectrum/checks.py
@@ -405,6 +405,9 @@ class ApiCheck:
     def collections(self):
         self._list_api('/collections', 'collection')
 
+    def profiles(self):
+        self._list_api('/profiles', 'profile')
+
     def _list_api(self, path, entity):
         url = "%s%s" % (self._host, path)
         response = requests.get(url, headers=self._base_headers({'Accept': 'application/vnd.elife.%s-list+json; version=1' % entity}))
