e0a8b0696f2ed5d6fc0b44984c7b570fa04fc999
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-10-24 08:50:12 +0100

    Fix Python version

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 set -e
 source prerequisites.sh
 
-virtualenv venv
+if [ ! -e "venv/bin/python2.7" ]; then
+    echo "could not find venv/bin/python2.7, recreating venv"
+    rm -rf venv
+    virtualenv --python=python2.7 venv
+fi
+
 source venv/bin/activate
 pip install -r requirements.txt
