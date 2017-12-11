21c0b9e6db172c96f499434befd9b54add144059
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-11 16:30:31 +0000

    Rewriting detection of last run

--- spectrum/test_article.py
+++ spectrum/test_article.py
@@ -33,8 +33,8 @@ def test_article_multiple_ingests_of_the_same_version(generate_article, modify_a
     run2_start = datetime.now()
     modified_article = modify_article(article, replacements={'cytomegalovirus': 'CYTOMEGALOVIRUS'})
     _ingest(modified_article)
-    a = checks.DASHBOARD.ready_to_publish(id=article.id(), version=article.version(), run_after=run2_start)
-    run2 = a['versions']['1']['runs']['2']['run-id']
+    article_on_dashboard = checks.DASHBOARD.ready_to_publish(id=article.id(), version=article.version(), run_after=run2_start)
+    run2 = article_on_dashboard['versions'][str(article.version())]['runs']['2']['run-id']
     assert run2 != run1, "A new run should have been triggered"
     input.DASHBOARD.publish(id=article.id(), version=article.version(), run=run2)
     checks.API.wait_article(id=article.id(), title='Correction: Human CYTOMEGALOVIRUS IE1 alters the higher-order chromatin structure by targeting the acidic patch of the nucleosome')
@@ -183,10 +183,10 @@ def _feed_silent_correction(article):
     input.SILENT_CORRECTION_BUCKET.upload(article.filename(), article.id())
 
 def _wait_for_publishable(article, run_after):
-    if run_after:
-        checks.DASHBOARD.ready_to_publish(id=article.id(), version=article.version(), run_after=run_after)
-    # fails quite often but is now late in the process, can we make an intermediate check?
-    run = a['versions'][str(article.version())]['runs']['1']['run-id']
+    article_on_dashboard = checks.DASHBOARD.ready_to_publish(id=article.id(), version=article.version(), run_after=run_after)
+    runs = article_on_dashboard['versions'][str(article.version())]['runs']
+    last_run_number = max([int(i) for i in runs.keys()])
+    run = runs[str(last_run_number)]['run-id']
     for each in article.figure_names():
         checks.IMAGES_PUBLISHED_CDN_BUCKET.of(id=article.id(), figure_name=each, version=article.version())
     checks.XML_PUBLISHED_CDN_BUCKET.of(id=article.id(), version=article.version())
