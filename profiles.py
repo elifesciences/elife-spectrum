abcb271081f7c0872c39a5988506ccffceaee094
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-30 14:45:48 +0000

    Loading of all profile pages
    (pagination doesn't work at the moment)

--- profiles.py
+++ profiles.py
@@ -3,13 +3,26 @@ from sys import argv, exit
 import fileinput
 import requests
 
+JOURNAL = checks.JOURNAL.with_query_string("open-sesame")
 
 if __name__ == '__main__':
-    anonymous_session = input.JOURNAL.session()
-    anonymous_session.enable_feature_flag()
+    pages = []
     for line in fileinput.input():
         profile_id = line.strip("\n")
         checks.LOGGER.info("Profile: %s", profile_id) 
+
+        # basic check on profile existence
         checks.API.profile(profile_id)
-        anonymous_session.check('/profiles/%s' % profile_id)
+
+        # first page of annotations on the API
+        checks.API.annotations(profile_id)
+
+        pages.append((load.JournalListing(JOURNAL, "/profiles/%s" % profile_id), 1))
+    load_strategy = load.AllOf(pages)
+    while True:
+        try:
+            load_strategy.run()
+        except (AssertionError, RuntimeError, ValueError, checks.UnrecoverableError, requests.exceptions.ConnectionError) as e:
+            load.LOGGER.exception("Error in loading (%s)", e.message)
+
 
