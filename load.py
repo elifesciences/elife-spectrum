c179e042e38fe3effc51089f392f597ff45f5308
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-31 15:33:28 +0000

    Switch load script to use argparse

--- load.py
+++ load.py
@@ -1,19 +1,20 @@
+from argparse import ArgumentParser
 from spectrum import load, checks
 from sys import argv, exit
 import requests
 
 if __name__ == '__main__':
-    if len(argv) < 2:
-        load.LOGGER.error("No strategy specified. Usage: load.py STRATEGY [ITERATIONS]")
-        exit(1)
-    strategy = argv[1]
-    load.LOGGER.info("Loading strategy %s", strategy)
+    parser = ArgumentParser(description="Load tests Journal")
+    parser.add_argument('strategy', type=str, help='Strategy to use e.g. JOURNAL_ALL')
+    parser.add_argument('--limit', type=int, default=None, help='The maximum number of checks to make before stopping')
+    arguments = parser.parse_args()
+    load.LOGGER.info("Loading strategy %s", arguments.strategy)
     try:
-        load_strategy = getattr(load, strategy)
+        load_strategy = getattr(load, arguments.strategy)
     except AttributeError:
-        load.LOGGER.error("Unknown strategy %s", strategy)
+        load.LOGGER.error("Unknown strategy %s", arguments.strategy)
         exit(2)
-    limit = load.Limit(int(argv[2]) if len(argv) > 2 else None)
+    limit = load.Limit(arguments.limit)
     load.LOGGER.info("Setting iterations limit %s", limit)
 
     limit.run(load_strategy)
