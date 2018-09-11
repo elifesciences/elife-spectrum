5f4c7fb82f85efff4eaed911caa01b98b2d7e51a
nlisgo <nathan@aeopus.com>
2018-09-11 12:33:17 +0100

    Switch to podcast episode test as listed in search

--- spectrum/input.py
+++ spectrum/input.py
@@ -120,15 +120,18 @@ class JournalCmsSession:
         # but in checks module
         # TODO: return id and/or node id
 
-    def create_job_advert(self, title, text='Lorem ipsum'):
-        create_url = "%s/node/add/job_advert" % self._host
+    def create_podcast_episode(self, title, image, uri, chapter_title):
+        create_url = "%s/node/add/podcast_episode" % self._host
         create_page = self._browser.get(create_url)
         form = mechanicalsoup.Form(create_page.soup.form)
         form.input({'title[0][value]': title})
-        form.textarea({'field_job_advert_role_summary[0][value]': text})
 
-        LOGGER.info("Saving form")
-        # not sure why, but `data` here is necessary
+        form.attach({'files[field_image_0]': image})
+        LOGGER.info("Attaching image")
+
+        form.input({'field_episode_mp3[0][uri]': uri})
+        form.input({'field_episode_chapter[form][inline_entity_form][title][0][value]': chapter_title})
+
         response = self._browser.submit(form, create_page.url, data={'op': 'Save'})
         # requests follows redirects by default
         _assert_html_response(response)
--- spectrum/test_journal_cms.py
+++ spectrum/test_journal_cms.py
@@ -1,5 +1,6 @@
 "Tests that go through Journal CMS and propagate content to the rest of the system"
 import pytest
+import requests
 from spectrum import input
 from spectrum import checks
 
@@ -13,10 +14,17 @@ def test_content_type_propagates_to_other_services():
     journal_cms_session = input.JOURNAL_CMS.login()
 
     invented_word = input.invented_word()
-    title = 'Spectrum blog article: %s' % invented_word
-    text = 'Lorem ipsum... %s' % title
-    journal_cms_session.create_job_advert(title=title, text=text)
+    title = 'Spectrum podcast episode: %s' % invented_word
+
+    journal_cms_session.create_podcast_episode(title=title, image='./spectrum/fixtures/king_county.jpg', uri='https://cdn.elifesciences.org/podcast-episode.mp3', chapter_title='Chapter 1')
     result = checks.API.wait_search(invented_word)
     assert result['total'] == 1, "There should only be one result containing this word"
-    assert result['items'][0]['title'] == title, "The title of the job advert found through search is incorrect"
+    assert result['items'][0]['title'] == title, "The title of the podcast episode found through search is incorrect"
+    number = result['items'][0]['number']
+    podcast_episode = checks.API.podcast_episode(number)
+    # TODO: transition to IIIF and use a IiifCheck object
+    image_url = blog_article['image']['banner']['source']['uri']
+    response = requests.head(image_url)
+    checks.LOGGER.info("Found %s: %s", image_url, response.status_code)
+    assert response.status_code == 200, "Image %s is not loading" % image_url
 
