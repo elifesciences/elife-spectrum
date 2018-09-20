bd25b3afb63a694bc713bf01cbd5c5886bb12817
Giorgio Sironi <g.sironi@elifesciences.org>
2018-09-20 15:29:43 +0100

    Ingest an article with a digest

--- spectrum/test_digest.py
+++ spectrum/test_digest.py
@@ -1,4 +1,5 @@
 import pytest
+from spectrum import articles
 from spectrum import checks
 from spectrum import input
 
@@ -6,10 +7,13 @@ SIMPLEST_ARTICLE_ID = 15893
 
 @pytest.mark.bot
 #@pytest.mark.digests
-def test_digest_lifecycle(generate_digest):
+def test_digest_lifecycle(generate_digest, generate_article):
     digest = generate_digest(SIMPLEST_ARTICLE_ID)
     input.DIGESTS_BUCKET.upload(digest.filename(), id=digest.article_id())
     checks.BOT_EMAILS.wait_email(subject='Digest: Anonymous_%s' % digest.article_id())
     checks.BOT_INTERNAL_DIGEST_OUTBOX_DOC.of(id=digest.article_id())
     checks.BOT_INTERNAL_DIGEST_OUTBOX_JPG.of(id=digest.article_id())
     #checks.DIGEST_JPG_PUBLISHED_CDN_BUCKET.of(id=digest.article_id())
+
+    article = generate_article(template_id=SIMPLEST_ARTICLE_ID, article_id=digest.article_id())
+    articles.ingest(article)
