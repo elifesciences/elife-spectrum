dc9fb750dfc2355e50ed0bfe92341d020ba6ff1a
Giorgio Sironi <g.sironi@elifesciences.org>
2018-10-03 11:47:38 +0100

    Enable check on digest published image

--- spectrum/test_digest.py
+++ spectrum/test_digest.py
@@ -15,7 +15,7 @@ def test_digest_lifecycle(generate_digest, generate_article, modify_article):
     checks.BOT_EMAILS.wait_email(subject='Digest: Anonymous_%s' % digest.article_id())
     checks.BOT_INTERNAL_DIGEST_OUTBOX_DOC.of(id=digest.article_id())
     checks.BOT_INTERNAL_DIGEST_OUTBOX_JPG.of(id=digest.article_id())
-    #checks.DIGEST_JPG_PUBLISHED_CDN_BUCKET.of(id=digest.article_id())
+    checks.DIGEST_JPG_PUBLISHED_CDN_BUCKET.of(id=digest.article_id())
 
     # article ingestion
     article = generate_article(template_id=DIGEST_ARTICLE_ID, article_id=digest.article_id())
