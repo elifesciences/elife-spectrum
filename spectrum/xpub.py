e82741e524f3caa2a2a271b20feae92b0e7aca64
Giorgio Sironi <g.sironi@elifesciences.org>
2019-01-14 12:30:26 +0000

    Introduce wait specific for file processing

--- spectrum/xpub.py
+++ spectrum/xpub.py
@@ -72,6 +72,9 @@ class XpubInitialSubmissionFilesPage(PageObject):
     CSS_EDITOR_COVER_LETTER = '#coverLetter [contentEditable=true]'
     CSS_INPUT_MANUSCRIPT = '[data-test-id=upload]>input'
     CSS_UPLOAD_INSTRUCTIONS = 'p[data-test-conversion="completed"]'
+    # occasionally long xpub file uploads,
+    # due to sciencebeam metadata extraction
+    TIMEOUT_FILE_PROCESSING = 60
 
     def populate_required_fields(self):
         self._send_input_to(self.CSS_EDITOR_COVER_LETTER, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'cover letter')
@@ -83,7 +86,7 @@ class XpubInitialSubmissionFilesPage(PageObject):
         return XpubInitialSubmissionSubmissionPage(self._driver)
 
     def _wait_for_upload_and_conversion(self):
-        instructions = self._driver.find_element_by_css_selector(self.CSS_UPLOAD_INSTRUCTIONS)
+        instructions = WebDriverWait(self._driver, self.TIMEOUT_FILE_PROCESSING).until(lambda driver: self._driver.find_element_by_css_selector(self.CSS_UPLOAD_INSTRUCTIONS))
         LOGGER.info("Found instructions: %s", instructions.text)
 
 class XpubInitialSubmissionSubmissionPage(PageObject):
