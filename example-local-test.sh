615ff6ed54b5e103f640da02831a719504e84c71
Luke <lsh-0@users.noreply.github.com>
2021-06-10 14:57:40 +0930

    example-local-test.sh, improved examples

--- example-local-test.sh
+++ example-local-test.sh
@@ -3,9 +3,15 @@ set -e
 source venv/bin/activate
 
 export SPECTRUM_PROCESSES=1 # run tests serially
-export SPECTRUM_ENVIRONMENT=continuumtest  # required
+export SPECTRUM_ENVIRONMENT=continuumtest  # or end2end. required.
 export SPECTRUM_LOG_LEVEL=DEBUG  # more output
-export SPECTRUM_TIMEOUT=60  # speeds up errors
+export SPECTRUM_TIMEOUT=60  # speeds up errors, ~300 is typically best
 
-#python -m pytest -s spectrum/test_api.py
-python -m pytest -vvv --capture=no spectrum/test_journal_cms.py
+# run all tests in spectrum/test_journal_cms.py, ignoring anything on stdout
+#python -m pytest -vvv --capture=no spectrum/test_journal_cms.py
+
+# run all tests marked with 'journal_cms'
+#python -m pytest -vvv -m journal_cms
+
+# run the 'test_adding_article_fragment' test in 'spectrum/test_article.py'
+#python -m pytest -vvv spectrum/test_article.py::test_adding_article_fragment 
