b1165575fe3c15ba26f027fbf1f3e28e67fb85da
Luke <lsh-0@users.noreply.github.com>
2021-08-31 11:07:57 +0930

    new kitchen sink, syncs other assets with xml-mapping kitchen sink

--- example-local-test.sh
+++ example-local-test.sh
@@ -3,7 +3,7 @@ set -e
 source venv/bin/activate
 
 export SPECTRUM_PROCESSES=1 # run tests serially
-export SPECTRUM_ENVIRONMENT=continuumtest  # or end2end. required.
+export SPECTRUM_ENVIRONMENT=end2end  # or end2end. required.
 export SPECTRUM_LOG_LEVEL=DEBUG  # more output
 export SPECTRUM_TIMEOUT=60  # speeds up errors, ~300 is typically best
 
@@ -15,3 +15,5 @@ export SPECTRUM_TIMEOUT=60  # speeds up errors, ~300 is typically best
 
 # run the 'test_adding_article_fragment' test in 'spectrum/test_article.py'
 #python -m pytest -vvv spectrum/test_article.py::test_adding_article_fragment 
+
+python -m pytest -vvv spectrum/test_article.py::test_searching_for_a_new_article
