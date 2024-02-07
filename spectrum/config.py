5a81ad5944329c3801e7ef7a6e3f8cbfe3bbdf1a
Luke <2142748+lsh-0@users.noreply.github.com>
2024-02-07 12:32:44 +1030

    config, sets a global default user agent. this is so the default user agent 'python-requests' doesn't get blocked by the firewall.

--- spectrum/config.py
+++ spectrum/config.py
@@ -4,7 +4,7 @@ contains no tests to be run."""
 
 import os
 from configparser import RawConfigParser
-
+import requests
 import urllib3
 
 CONFIG = RawConfigParser()
@@ -13,10 +13,16 @@ ENV = os.environ['SPECTRUM_ENVIRONMENT'] if 'SPECTRUM_ENVIRONMENT' in os.environ
 COMMON = dict(CONFIG.items('common'))
 SETTINGS = dict(CONFIG.items(ENV))
 
+ELIFE_SPECTRUM_USER_AGENT = 'elife-spectrum (https://elifesciences.org/elife-spectrum)'
 GOOGLEBOT_USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+
 # https://urllib3.readthedocs.io/en/latest/user-guide.html#ssl would solve the warning,
 # but it is `requests` using `urllib3` here so unclear how to apply that guide
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# lsh@2024-02-07: added custom global user agent as UserAgents containing 'python-requests' have been added to firewall.
+# set a global default user agent by monkey patching default user agent function.
+requests.utils.default_user_agent = lambda _: ELIFE_SPECTRUM_USER_AGENT
 
 if __name__ == '__main__':
     for section in CONFIG.sections():
